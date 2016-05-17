2fac56f0b64c0c7adc6802140bf908528ce69baa
oppo545 <oppo545@qq.com>
2016-05-17 15:58:53 +0800

    啊啊啊啊

--- ServerCenterLis/Command/ProtocolFormat_23.cs
+++ ServerCenterLis/Command/ProtocolFormat_23.cs
@@ -1289,7 +1289,7 @@ namespace ServerCenterLis
                 #region  状态信息上报
                 else if (strindex.Equals("03"))
                 {
-                    session.cls_vlss = session.getClsVlsS();  //4561 啊实打实打算
+                    session.cls_vlss = session.getClsVlsS();  //4561 啊实打实打算阿道夫
                     //采集时间
                     session.cls_bjdb.Date = PublicMethods.GetGMT8Data(desc.Substring(0, 17), 1);
                     //状态标志
